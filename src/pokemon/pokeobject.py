a37081f972370f52c18a9ba8b14794176866c5e7
Gene Ni <geneisjuan@gmail.com>
2023-08-23 23:58:44 -0400

    working on overhauling pokeobject

--- src/pokemon/pokeobject.py
+++ src/pokemon/pokeobject.py
@@ -1,233 +1,327 @@
+from typing import List, Dict
 
+
+class Type:
+    def __init__(self, _type: str) -> None:
+        self._type = _type
+        
+    @property
+    def type_name(self) -> str:
+        return self._type
+        
 class PokeObject:
     
-    class PokemonEntry:
+    class Version:
         
-        def __init__(self, name, entries) -> None:
+        class PokedexData:
             
-            self._name = name
-            self._entries = entries
+            class Ability:
+                
+                def __init__(self, ability: dict) -> None:
+                    self._href: str = ability['href']
+                    self._effect: str = ability['effect']
+                    self._hidden: bool = ability['hidden']
+                    
+                    
+                @property
+                def href(self) -> str:
+                    return self.href
+                
+                @property
+                def effect(self) -> str:
+                    return self._effect
+                
+                @property
+                def hidden(self) -> bool:
+                    return self._hidden
+            
+            def __init__(self, pokedex_data: dict) -> None:
+                
+                self._national_no: int = int(pokedex_data['National No.'])
+                self._type: List[Type] = pokedex_data['Type']
+                self._species: str = pokedex_data['Species']
+                self._height: str = pokedex_data['Height']
+                self._weight: str = pokedex_data['Weight']
+                self._abilities: Dict[str, self.Ability] = {}
+                
+                for ability in pokedex_data["Abilities"]:
+                    self._abilities[ability] = self.Ability(pokedex_data['Abilities'][ability])
+                    
+            @property
+            def national_no(self) -> int:
+                return self._national_no
+            
+            @property
+            def poke_types(self) -> List[Type]:
+                return self._type
+            
+            @property
+            def species(self) -> str:
+                return self._species
+            
+            @property
+            def height(self) -> str:
+                return self._height
+            
+            @property
+            def weight(self) -> str:
+                return self._weight
+            
+            @property
+            def abilities(self) -> Dict[str, Ability]:
+                return self._abilities
+            
+        class TrainingData:
+            
+            def __init__(self, training_data: dict) -> None:
+                
+                self._ev_yield: str = training_data['EV yield']
+                self._catch_rate: int = int(training_data['Catch rate'])
+                self._basefriendship: int = int(training_data['BaseFriendship'])
+                self._base_exp: int = int(training_data['Base Exp.'])
+                self._growth_rate: str = training_data['Growth Rate']
+            
+            @property
+            def ev_yield(self) -> str:
+                return self._ev_yield
+            
+            @property
+            def catch_rate(self) -> int:
+                return self._catch_rate
+            
+            @property
+            def base_friendship(self) -> int:
+                return self._basefriendship
+            
+            @property
+            def base_exp(self) -> int:
+                return self._base_exp
+            
+            @property
+            def growth_rate(self) -> str:
+                return self._growth_rate
+
+        class BreedingData:
+            
+            def __init__(self, training_data: dict) -> None:
+                
+                self._gender: str = training_data['Gender']
+                
+            @property
+            def gender(self) -> str:
+                return self._gender
+                      
+        class BaseStats:
+            
+            class Stat:
+                
+                def __init__(self, stat: dict) -> None:
+                    
+                    self._base = int(stat['Base'])
+                    self._min = int(stat['Min'])
+                    self._max = int(stat['Max'])
+                    
+                @property
+                def base(self) -> int:
+                    return self._base
+                
+                @property
+                def minimum(self) -> int:
+                    return self._min
+                
+                @property
+                def maximum(self) -> int:
+                    return self._max
+            
+            def __init__(self, base_stats: dict) -> None:
+                
+                self._hp = self.Stat(base_stats['HP'])
+                self._attack = self.Stat(base_stats['Attack'])
+                self._defense = self.Stat(base_stats['Defense'])
+                self._sp_atk = self.Stat(base_stats['Sp. Atk'])
+                self._sp_def = self.Stat(base_stats['Sp. Def'])
+                self._speed = self.Stat(base_stats['Speed'])
+                self._total = self.Stat(base_stats['Total'])
+                
+            @property
+            def hp(self) -> Stat:
+                return self._hp
+            
+            @property
+            def attack(self) -> Stat:
+                return self._attack
+            
+            @property
+            def defense(self) -> Stat:
+                return self._defense
+            
+            @property
+            def sp_atk(self) -> Stat:
+                return self._sp_atk
+            
+            @property
+            def sp_def(self) -> Stat:
+                return self._sp_def
+            
+            @property
+            def speed(self) -> Stat:
+                return self._speed
+            
+            @property
+            def total(self) -> Stat:
+                return self._total
+        
+        class DefenseStats:
+            
+            class DefenseStat:
+                
+                def __init__(self, defenses: dict) -> None:
+                    
+                    pass
+            
+            def __init__(self, defense_stats: dict) -> None:
+                
+                pass
+            
+        class Images:
+            
+            def __init__(self, images: dict) -> None:
+                
+                self._discord_image: str = images['discord_image']
+                self._discord_sprite: str = images['discord_sprite']
+                
+            @property
+            def discord_image(self) -> str:
+                return self._discord_image
+            
+            @property
+            def discord_sprite(self) -> str:
+                return self._discord_sprite
+                
+        
+        def __init__(self, version: dict) -> None:
+            
+            self._name: str = version['name']
+            self._data: dict = version['data']
             
         @property
         def name(self) -> str:
-            
             return self._name
         
         @property
-        def entries(self) -> dict:
+        def pokedex_data(self) -> PokedexData:
+            return self.PokedexData(self._data['pokedex_data'])
+        
+        @property
+        def training_data(self) -> TrainingData:
+            return self.TrainingData(self._data['training_data'])
+        
+        @property
+        def breeding(self) -> BreedingData:
+            return self.BreedingData(self._data['breeding_data'])
+        
+        @property
+        def base_stats(self) -> BaseStats:
+            return self.BaseStats(self._data['base_stats'])
+        
+        @property
+        def defense_stats(self) -> DefenseStats:
+            return self.DefenseStats(self._data['defense_stats'])
+        
+        @property
+        def images(self) -> Images:
+            return self.Images(self._data['images'])
+    
+    class Evolution:
+        
+        class EvolutionPokemon:
             
-            return self._entries
-        
-    class Version:
-        
-        class Stat:
-            def __init__(self, base: int, min_: int, max_: int) -> None:
-                self._base = base
-                self._min = min_
-                self._max = max_
+            def __init__(self, pokemon: dict) -> None:
+                
+                self._name: str = pokemon['name']
+                self._national_no: int = int(pokemon['nationalNo'][1:])
+                self._type: List[Type] = [Type(poke_type) for poke_type in pokemon['type']]
+                self._nickname: str | None = pokemon['nickname']
                 
             @property
-            def base(self):
-                
-                return self._base
+            def name(self) -> str:
+                return self.name
             
             @property
-            def minimum(self):
-                
-                return self._min
+            def national_no(self) -> int:
+                return self._national_no
             
             @property
-            def maximum(self):
-                
-                return self._max
-            
-        class Ability:
-            def __init__(self, ability: dict) -> None:
-                self.ability = ability
+            def poke_type(self) -> List[Type]:
+                return self._type
             
             @property
-            def effect(self) -> str:
-                
-                return self.ability['effect']
-            
-            @property
-            def hidden(self) -> bool:
-                
-                return self.ability['hidden']
-                
-            
-        def __init__(self, name: str, version: dict) -> None:
-            
-            self.version_name = name
-            self.__getPokedexData(version['pokedex_data'])
-            self.__getTrainingData(version['training_data'])
-            self.__getBreedingData(version['breeding_data'])
-            self.__getBaseStats(version['base_stats'])
-            self.__getDefenseStats(version['defense_stats'])
-        
-        def __getPokedexData(self, pokedex_data: dict):
-            
-            self._national_number = pokedex_data['National No.']
-            self._type = pokedex_data['Type']
-            self._species = pokedex_data['Species']
-            self._height = pokedex_data['Height']
-            self._weight = pokedex_data['Weight']
-            self._abilities = {}
-            for ability in pokedex_data['Abilities']:
-                self._abilities[ability] = self.Ability(pokedex_data['Abilities'][ability])
-                
-        def __getTrainingData(self, training_data: dict):
-            self._ev_yield = training_data['EV yield']
-            self._catch_rate = training_data['Catch rate'] if training_data['Catch rate'] != '-' else None
-            self._base_friendship = training_data['BaseFriendship'] if training_data['BaseFriendship'] != '-' else None
-            self._base_xp = training_data['Base Exp.'] if training_data['Base Exp.'] != '-' else None
-            self._growth_rate = training_data['Growth Rate']
-        
-        def __getBreedingData(self, breeding_data: dict):
-            self._gender = breeding_data['Gender'].split(",")
-            
-        def __getBaseStats(self, base_stats: dict):
-            
-            self._stats = {}
-            
-            for s in base_stats:
-                if s == 'Total':
-                    self.stats[s] = base_stats[s]
-                else:
-                    self.stats[s] = self.Stat(
-                        base=base_stats[s]["Base"],
-                        max_=base_stats[s]["Max"],
-                        min_=base_stats[s]["Min"]
-                        )
-        
-        def __getDefenseStats(self, defense_stats):
-            self._defense_stats = defense_stats
+            def nickname(self) -> str | None:
+                return self._nickname
 
-        @property
-        def national_number(self):
+        def __init__(self, evolution) -> None:
             
-            return self._national_number
+            self._from = self.EvolutionPokemon(evolution['from'])
+            self._to = self.EvolutionPokemon(evolution['to'])
+            self._requirement: str = evolution['requirement']
+            self._shed: self.EvolutionPokemon | None = evolution['shed'] if evolution['shed'] is None else self.EvolutionPokemon(evolution['shed'])
+            
+        @property
+        def evolution_from(self) -> EvolutionPokemon:
+            return self._from
         
         @property
-        def pokemon_type(self):
-            
-            return self._type
+        def evolution_to(self) -> EvolutionPokemon:
+            return self._to
         
         @property
-        def species(self):
-            
-            return self._species
+        def requirement(self) -> str:
+            return self._requirement
         
         @property
-        def height(self):
+        def shed(self) -> EvolutionPokemon | None:
+            return self._shed
             
-            return self._height
+    class AttackCategory:
         
-        @property
-        def weight(self):
+        def __init__(self, category_name, category_moves) -> None:
             
-            return self._weight
-        
-        @property
-        def abilities(self):
             
-            return self._abilities
-        
-        @property
-        def ev_yield(self):
-            
-            return self._ev_yield
-        
-        @property
-        def catch_rate(self):
-            
-            return self._catch_rate
-        
-        @property
-        def base_friendship(self):
-            
-            return self._base_friendship
-        
-        @property
-        def base_xp(self):
-            
-            return self._base_xp
-        
-        @property
-        def growth_rate(self):
-            
-            return self._growth_rate
-
-        @property
-        def gender(self):
-            
-            return self._gender
-        
-        @property
-        def stats(self):
-            
-            return self._stats
-        
-        @property
-        def defense(self):
-            
-            return self._defense_stats
-
-
-
-
+    
+    
     def __init__(self, pokemon_raw: dict) -> None:
         
-        
-        self._name: str = pokemon_raw['name']
-        self._versions: list = []
-        
-        for alt_num in pokemon_raw['versions']:
-            self.versions.append(self.Version(name=alt_num['name'], version=alt_num['data']))
-            
-        self._evolution_tree: list = pokemon_raw['evo_stats']
-        
-        self._attacks_data: dict = pokemon_raw['attacks_data']
-        
-        self._pokedex_entries = []
-        
-        for e in pokemon_raw['entries']:
-            self._pokedex_entries.append(self.PokemonEntry(e, pokemon_raw['entries'][e]))
-            
-        self._discord_image = pokemon_raw['discord_image']
-        self._discord_sprite = pokemon_raw['discord_sprite']
+        self._versions = self.__pokemon_version_processor(pokemon_raw['versions'])
+        self._evo_stats = self.__pokemon_evolution_processor(pokemon_raw['evo_stats'])
+        self._attacks_data = pokemon_raw['attacks_data']
+        self._entries = pokemon_raw['entries']
+        self._name = pokemon_raw['name']
         
     @property
-    def evolution_tree(self):
-        
-        return self._evolution_tree
-    
-    @property
-    def attacks(self):
-        
-        return self._attacks_data
-    
-    @property
-    def pokedex_entries(self):
-        
-        return self._pokedex_entries
-    
-    @property
-    def name(self):
-        
-        return self._name
-    
-    @property
-    def versions(self):
-        
+    def versions(self) -> List[Version]:
         return self._versions
     
     @property
-    def discord_image(self):
+    def evolutions(self) -> List[Evolution]:
+        return self._evo_stats
         
-        return self._discord_image
+    def __pokemon_version_processor(self, pokemon_versions_raw: dict) -> List[Version]:
+        
+        return [self.Version(version) for version in pokemon_versions_raw]
     
-    @property
-    def discord_sprite(self):
+    def __pokemon_evolution_processor(self, pokemon_evolution_raw: list) -> List[Evolution]:
         
-        return self._discord_sprite
+        return [self.Evolution(evolution) for evolution in [evo_chart for evo_chart in pokemon_evolution_raw]]
+    
+    def __pokemon_attacks_processor(self, pokemon_attacks_raw: dict) -> List[AttackCategory]:
+        
+        return [self.AttackCategory(category, pokemon_attacks_raw[category]) for category in pokemon_attacks_raw]
+    
+    def __pokemon_entries_processor(self, pokemon_entires_raw: dict):
+        
+        return
+
+pikachu = PokeObject({})
+
+for version in pikachu.versions:
+    version.name
